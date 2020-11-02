00652c30c3dbdb69bfb09102d521fdb22e75e5d8
22mahmoud <mahmoudss1997@gmail.com>
2020-11-02 13:24:29 +0200

    fix fonts and snap

--- config.def.h
+++ config.def.h
@@ -7,11 +7,11 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
-static const unsigned int snap      = 60;       /* snap pixel */
+static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:truetype:scale=11", "Tajawal:size=10"  };
+static const char *fonts[]          = { "monospace:size=10", "Tajawal:size=10"  };
 static const char dmenufont[]       = "monospace:size=10";
 
 // background color
