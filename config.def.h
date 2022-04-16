6505b6094d8c991de3286f9d79b9eb985b32b7ad
22mahmoud <hello@mahmoudashraf.dev>
2022-04-17 01:35:42 +0200

    Squashed commit of the following: fullgaps
    commit 874df6383a1d74db49dc005021462fbb75ceeaf4
    Author: 22mahmoud <hello@mahmoudashraf.dev>
    Date:   Sun Apr 17 01:21:13 2022 +0200
    
        apply fullgaps patch

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
+static unsigned int gappx     = 5;        /* gaps between windows */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char font[]            = "monospace:size=10";
@@ -110,6 +111,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
