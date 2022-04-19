eac70cf5054eef7aaea8db2d61264e7d66d6579a
22mahmoud <hello@mahmoudashraf.dev>
2022-04-19 15:14:43 +0200

    Squashed commit of the following: swallow patch
    commit 8a677539af2553d7e3813b3392a9884208364a9f
    Author: 22mahmoud <hello@mahmoudashraf.dev>
    Date:   Tue Apr 19 15:07:10 2022 +0200
    
        apply swallow patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char font[]            = "monospace 10";
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -46,9 +47,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance   title       tags mask     isfloating   monitor */
-	{ NULL,		  "spterm",		NULL,		    SPTAG(0),		  1,			    -1 },
-	{ NULL,		  "spfm",		  NULL,		    SPTAG(1),		  1,			    -1 },
+	/* class     instance   title           tags mask   isfloating  isterminal  noswallow   monitor */
+	{ "St",      NULL,      NULL,           0,          0,          1,           0,         -1 },
+	{ NULL,		  "spterm",		NULL,		        SPTAG(0),	  1,			    1,           1,         -1 },
+	{ NULL,		  "spfm",		  NULL,		        SPTAG(1),	  1,			    1,           1,         -1 },
+	{ NULL,      NULL,     "Event Tester",  0,          0,          0,           1,         -1 }, /* xev */
 };
 
 /* layout(s) */
