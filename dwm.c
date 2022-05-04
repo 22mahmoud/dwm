2920d206e0d29c24287e534c163f5c2fffe832ce
22mahmoud <hello@mahmoudashraf.dev>
2022-05-04 22:59:47 +0200

    remove unused line of code

--- dwm.c
+++ dwm.c
@@ -1008,7 +1008,6 @@ drawbar(Monitor *m)
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i, False);
-		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i, False);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
