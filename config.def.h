ebe65c7f492bccb9d3b8161094ecfe4296e7d281
22mahmoud <hello@mahmoudashraf.dev>
2022-05-11 02:07:47 +0200

    custom systray icon size

--- config.def.h
+++ config.def.h
@@ -22,6 +22,7 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const unsigned int systrayiconsize = 20; /* systray icon size in px */
 static const int showsystray        = 1;     /* 0 means no systray */
 
 /* 
--- dwm.c
+++ dwm.c
@@ -2712,24 +2712,17 @@ updatestatus(void)
 void
 updatesystrayicongeom(Client *i, int w, int h)
 {
-	if (i) {
+  if (!i)
+    return;
+
+  applysizehints(i, &(i->x), &(i->y), &(i->w), &(i->h), False);
+  if (systrayiconsize >= bh) {
+    i->w = bh;
 		i->h = bh;
-		if (w == h)
-			i->w = bh;
-		else if (h == bh)
-			i->w = w;
-		else
-			i->w = (int) ((float)bh * ((float)w / (float)h));
-		applysizehints(i, &(i->x), &(i->y), &(i->w), &(i->h), False);
-		/* force icons into the systray dimensions if they don't want to */
-		if (i->h > bh) {
-			if (i->w == i->h)
-				i->w = bh;
-			else
-				i->w = (int) ((float)bh * ((float)i->w / (float)i->h));
-			i->h = bh;
-		}
-	}
+	} else {
+    i->w = systrayiconsize;
+    i->h = systrayiconsize;
+  }
 }
 
 void
@@ -2807,8 +2800,11 @@ updatesystray(void)
 		XMapRaised(dpy, i->win);
 		w += systrayspacing;
 		i->x = w;
-		XMoveResizeWindow(dpy, i->win, i->x, 0, i->w, i->h);
 		w += i->w;
+    if (systrayiconsize >= bh) i->y = 0;
+    else
+      i->y = (bh - systrayiconsize) / 2;
+    XMoveResizeWindow(dpy, i->win, i->x, i->y, i->w, i->h);
 		if (i->mon != m)
 			i->mon = m;
 	}
