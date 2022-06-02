d870e5bfd102842635c7d9b8398840857c56e54c
22mahmoud <hello@mahmoudashraf.dev>
2022-06-02 11:31:28 +0200

    use config instead share dir for autostart

--- dwm.c
+++ dwm.c
@@ -378,7 +378,7 @@ static const char autostartblocksh[] = "autostart_blocking.sh";
 static const char autostartsh[] = "autostart.sh";
 static Systray *systray = NULL;
 static const char dwmdir[] = "dwm";
-static const char localshare[] = ".local/share";
+static const char config[] = ".config";
 static const char broken[] = "broken";
 static char stext[512];
 static int screen;
@@ -1903,7 +1903,7 @@ runautostart(void)
 {
 	char *pathpfx;
 	char *path;
-	char *xdgdatahome;
+	char *xdgconfighome;
 	char *home;
 	struct stat sb;
 
@@ -1912,23 +1912,23 @@ runautostart(void)
 		return;
 
 	/* if $XDG_DATA_HOME is set and not empty, use $XDG_DATA_HOME/dwm,
-	 * otherwise use ~/.local/share/dwm as autostart script directory
+	 * otherwise use ~/.config/dwm as autostart script directory
 	 */
-	xdgdatahome = getenv("XDG_DATA_HOME");
-	if (xdgdatahome != NULL && *xdgdatahome != '\0') {
+	xdgconfighome = getenv("XDG_CONFIG_HOME");
+	if (xdgconfighome != NULL && *xdgconfighome != '\0') {
 		/* space for path segments, separators and nul */
-		pathpfx = ecalloc(1, strlen(xdgdatahome) + strlen(dwmdir) + 2);
+		pathpfx = ecalloc(1, strlen(xdgconfighome) + strlen(dwmdir) + 2);
 
-		if (sprintf(pathpfx, "%s/%s", xdgdatahome, dwmdir) <= 0) {
+		if (sprintf(pathpfx, "%s/%s", xdgconfighome, dwmdir) <= 0) {
 			free(pathpfx);
 			return;
 		}
 	} else {
 		/* space for path segments, separators and nul */
-		pathpfx = ecalloc(1, strlen(home) + strlen(localshare)
+		pathpfx = ecalloc(1, strlen(home) + strlen(config)
 		                     + strlen(dwmdir) + 3);
 
-		if (sprintf(pathpfx, "%s/%s/%s", home, localshare, dwmdir) < 0) {
+		if (sprintf(pathpfx, "%s/%s/%s", home, config, dwmdir) < 0) {
 			free(pathpfx);
 			return;
 		}
