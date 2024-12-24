53663c089a6813dd6582ca1d67128c6766c04bad
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2024-12-24 17:26:29 +0200

    config

--- config.def.h
+++ config.def.h
@@ -158,6 +158,7 @@ static const Rule rules[] = {
   RULE(.title = "Steam", .tags = 1 << 3),
   RULE(.class = "Steam", .title = "Friends List", .isfloating = 1, .tags = 1 << 3),
   RULE(.class = "bottles", .tags = 1 << 3),
+  RULE(.class = "Faugus-launcher", .tags = 1 << 3),
 
   RULE(.title = "Event Tester", .isfloating = 1, .noswallow = 1),
 };
